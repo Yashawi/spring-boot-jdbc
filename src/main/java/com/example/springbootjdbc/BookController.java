48820c60b85231860671e5fbb37e5a9c7a175533
yashaswisharan <yashaswisharan@10.1.10.24>
2019-06-14 22:21:13 -0400

    Initial spring jdbc web project

---
+++ src/main/java/com/example/springbootjdbc/BookController.java
@@ -0,0 +1,24 @@
+package com.example.springbootjdbc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/")
+	public ModelAndView home() {
+	ModelAndView mv = new ModelAndView("index");
+	return mv;
+
+}
+@RequestMapping(value="/listBooks")
+public ModelAndView getAllBooks() {
+	ModelAndView mv = new ModelAndView("list");
+	return mv;
+}
+
+}
